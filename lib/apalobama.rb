6aba8d04ae1948e4e6881a44c111d70ecbd66518
Baptiste Lafontaine <baptiste33@gmail.com>
2013-05-03 18:15:16 +0200

    Some work done

--- lib/apalobama.rb
+++ lib/apalobama.rb
@@ -5,12 +5,15 @@ require 'amphibian'
 
 module Apalobama
 	class CLI < Thor
+		attr_reader :current_amph
+
 		desc "list", "List host of given group"
 		method_option :group, :aliases => "-g", :desc => "Do the action on the whole group"
-		def list(url = nil)
-			group = options[:group]
-			if group and url.nil?
-			end
+		def list(url)
+			@current_amph = Amphibian::BalancerManager.new(url)
+			puts @current_amph.hosts_with_status
+		end
+		def listGroup(groupName)
 		end
 	end
 end
