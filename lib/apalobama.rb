3ef5a93434b7f37ac85d98820e5e3e1956ad9dcc
Baptiste Lafontaine <baptiste33@gmail.com>
2013-05-05 20:30:14 +0200

    Looks like a doable CLI interface

--- lib/apalobama.rb
+++ lib/apalobama.rb
@@ -1,19 +1,48 @@
-require 'apalobama/version'
-require 'thor'
+require 'uri'
 require 'yaml'
+
+require 'apalobama/version'
+
+require 'thor'
 require 'amphibian'
 
 module Apalobama
 	class CLI < Thor
-		attr_reader :current_amph
-
-		desc "list", "List host of given group"
-		method_option :group, :aliases => "-g", :desc => "Do the action on the whole group"
-		def list(url)
-			@current_amph = Amphibian::BalancerManager.new(url)
-			puts @current_amph.hosts_with_status
+		def initialize()
+			@amphibians = Hash.new
+			@config = YAML::load(File.open('/etc/apalobama.yml'))
 		end
-		def listGroup(groupName)
+
+		desc "list URL|GROUP", "List host of given group or URL"
+		def list(element)
+			loadElement(element)
+			@amphibians.each do |url, amphibian|
+				puts amphibian.hosts_with_status
+			end
+		end
+		
+		desc "disable URL|GROUP HOST", "Disable host for the given group or URL"
+		def disable(element, host)
+			loadElement(element)
+			@amphibians.each do |url, amphibian|
+				amphibian.disable_host host
+			end
+		end
+		
+		private
+		
+		def loadElement(element)
+			if url =~ /^#{URI::regexp}$/
+				@amphibians.store(url, Amphibian::BalancerManager.new(url))
+			else
+				if @config.has_key?(:groups) and @config.has_key?(element)
+					@config[:groups][element].each do |url|
+						@amphibians.store(url, Amphibian::BalancerManager.new(url))
+					end
+				else
+					raise Thor::Error, "Unknown group name"
+				end
+			end
 		end
 	end
 end
