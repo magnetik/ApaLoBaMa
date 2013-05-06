128b163090cce18b3343408def8deb0fd0521883
Baptiste Lafontaine <baptiste33@gmail.com>
2013-05-06 10:23:55 +0200

    Looks like 'list' command is working

--- lib/apalobama.rb
+++ lib/apalobama.rb
@@ -8,39 +8,46 @@ require 'amphibian'
 
 module Apalobama
 	class CLI < Thor
-		def initialize()
+		def initialize(*args)
+			super
 			@amphibians = Hash.new
 			@config = YAML::load(File.open('/etc/apalobama.yml'))
 		end
 
 		desc "list URL|GROUP", "List host of given group or URL"
 		def list(element)
-			loadElement(element)
+			loadElement element
 			@amphibians.each do |url, amphibian|
-				puts amphibian.hosts_with_status
+				puts "Hosts statuses for the balancer: #{url}"
+				amphibian.hosts_with_status.each do |balancer, status|
+					puts "  - #{balancer} => #{status}"
+				end
 			end
 		end
 		
 		desc "disable URL|GROUP HOST", "Disable host for the given group or URL"
 		def disable(element, host)
-			loadElement(element)
+			loadElement element
 			@amphibians.each do |url, amphibian|
-				amphibian.disable_host host
+				if not amphibian.hosts_with_status.has_key?(host)
+					raise Thor::Error, "Host '#{host}' not in balancer #{url}"
+				else if amphibian.hosts_with_status
+				end
 			end
 		end
 		
 		private
 		
 		def loadElement(element)
-			if url =~ /^#{URI::regexp}$/
-				@amphibians.store(url, Amphibian::BalancerManager.new(url))
+			if element =~ /^#{URI::regexp}$/
+				@amphibians.store(element, Amphibian::BalancerManager.new(element, true))
 			else
-				if @config.has_key?(:groups) and @config.has_key?(element)
-					@config[:groups][element].each do |url|
-						@amphibians.store(url, Amphibian::BalancerManager.new(url))
+				if @config.has_key?('groups') and @config['groups'].has_key?(element)
+					@config['groups'][element].each do |url|
+						@amphibians.store(url, Amphibian::BalancerManager.new(url, true))
 					end
 				else
-					raise Thor::Error, "Unknown group name"
+					raise Thor::Error, "Unknown group name: #{element}"
 				end
 			end
 		end
